713ce7f9a9971e8de4633c47a5d4e5b5a5050f41
joaouchoa <joaodep2@gmail.com>
2025-03-18 19:09:30 -0300

    feat: Criando os modelos e preparando para banco não relacional

---
+++ OrderProcessingService/OrderProcessingService.Domain/models/Client.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace OrderProcessingService.Domain.models
+{
+    public class Client
+    {
+        [BsonElement("idClient")]
+        public int IdClient { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("document")]
+        public string Document { get; set; }
+    }
+}
